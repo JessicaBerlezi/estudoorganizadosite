1aef961e5ac7f3d6e10019639fb731c5af6dc260
Berlezi <jessica.berlezi@gmail.com>
2026-05-02 18:01:47 -0300

    Conectando a endpoint de produção railway

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://estudoorganizadoapi.up.railway.app/mock-api'
+export const BASE_URL = 'https://estudoorganizadoapi.up.railway.app/mock-api'
 // export const BASE_URL = 'http://localhost:8080/api'
